213dc49a305e89b387a03986c6e63e04849b4880
sadiat moshood <sadiatm449@gmail.com>
2023-09-29 18:17:45 +0100

    Change the loader to blue

---
+++ src/weather.js
@@ -0,0 +1,17 @@
+import { Audio } from "react-loader-spinner";
+
+import React from "react";
+
+export default function Weather() {
+  return (
+    <Audio
+      height="80"
+      width="80"
+      radius="9"
+      color="blue"
+      ariaLabel="three-dots-loading"
+      wrapperStyle
+      wrapperClass
+    />
+  );
+}
