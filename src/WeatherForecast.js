459f089bdb1fb129a2779ad231e5a7eb576fb573
sadiat moshood <sadiatm449@gmail.com>
2023-11-01 09:33:00 +0100

    Updated the weatherForecast Url

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -38,7 +38,8 @@ export default function WeatherForecast(props) {
     let longitude = props.coordinates.lon;
 
     let latitude = props.coordinates.lat;
-    let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&appid=${apiKey}`;
+    let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=metric`;
+    // `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&appid=${apiKey}`;
     axios.get(apiUrl).then(handleResponse);
 
     return null;
